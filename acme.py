6e48be46d6d036350578f108541a14f07a47a0b6
tesseract314 <35466933+tesseract314@users.noreply.github.com>
2019-03-22 08:36:43 -0700

    Add stealability and explode methods

--- acme.py
+++ acme.py
@@ -9,3 +9,23 @@ class Product:
         self.weight = weight
         self.flammability = flammability
         self.identifier = identifier
+
+  def stealability(self):
+    price_over_weight = self.price / self.weight
+    
+    if price_over_weight < 0.5:
+      return "Not so stealable..."
+    elif price_over_weight >= 0.5 and price_over_weight < 1.0:
+      return "Kinda stealable."
+    else:
+      return "Very stealable!"
+    
+  def explode(self):
+    flam_times_weight = self.flammability * self.weight
+    
+    if flam_times_weight < 10:
+      return "...fizzle."
+    elif flam_times_weight >= 10 and flam_times_weight < 10:
+      return "...boom!"
+    else:
+      return "...BABOOM!!"
