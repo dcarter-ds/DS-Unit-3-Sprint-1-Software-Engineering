e78a1819b3954a0816041bc05abcd1e471ce8645
tesseract314 <35466933+tesseract314@users.noreply.github.com>
2019-03-22 08:50:58 -0700

    Add BoxingGlove subclass

--- acme.py
+++ acme.py
@@ -9,7 +9,7 @@ class Product:
         self.weight = weight
         self.flammability = flammability
         self.identifier = identifier
-
+        
   def stealability(self):
     price_over_weight = self.price / self.weight
     
@@ -29,3 +29,30 @@ class Product:
       return "...boom!"
     else:
       return "...BABOOM!!"
+    
+    
+class BoxingGlove(Product):
+  
+  def __init__(self, name=None, price=10, weight=10, flammability=0.5,
+               identifier= random.randint(1000000, 9999999)):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+    
+    
+    
+  def explode(self):
+    return "...it's a glove"
+  
+  def punch(self):
+    glove_weight = self.weight
+    
+    if glove_weight < 5:
+      return "That tickles."
+    
+    elif glove_weight >= 5 and glove_weight < 15:
+      return "Hey that hurt"
+    else:
+      return "OUCH!"
