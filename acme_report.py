7b02f3ff4bc1cac0fb986fdae66e349284f25a5b
tesseract314 <35466933+tesseract314@users.noreply.github.com>
2019-03-22 10:44:28 -0700

    Add pep8 requirements

--- acme_report.py
+++ acme_report.py
@@ -9,39 +9,40 @@ NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 def generate_products(num_products=30):
     rand_adj = []
     rand_noun = []
-    
+
     for _ in range(1, num_products+1):
-      rand_adj.append(random.choice(ADJECTIVES))
-  
+        rand_adj.append(random.choice(ADJECTIVES))
+
     for _ in range(1, num_products+1):
-      rand_noun.append(random.choice(NOUNS))
-    
+        rand_noun.append(random.choice(NOUNS))
+
     products = ["%s%02s" % t for t in zip(rand_adj, rand_noun)]
-    
+
     return products
 
+
 def inventory_report(products):
-  prices = []
-  weights = []
-  flams = []
+    prices = []
+    weights = []
+    flams = []
 
-  unique_products = len(set(products))
+    unique_products = len(set(products))
 
-  for i in range(0, 30):
-    prices.append(Product(products[i]).price)
-  
-  average_price = sum(prices) / 30
+    for i in range(0, 30):
+        prices.append(Product(products[i]).price)
 
-  for i in range(0, 30):
-    weights.append(Product(products[i]).weight)
-  
-  average_weights = sum(weights) / 30
+    average_price = sum(prices) / 30
 
-  for i in range(0, 30):
-    flams.append(Product(products[i]).flammability)
-  
-  average_flams = sum(flams) / 30
-     
+    for i in range(0, 30):
+        weights.append(Product(products[i]).weight)
+
+    average_weights = sum(weights) / 30
+
+    for i in range(0, 30):
+        flams.append(Product(products[i]).flammability)
+
+    average_flams = sum(flams) / 30
+
 
 if __name__ == '__main__':
     inventory_report(generate_products())
